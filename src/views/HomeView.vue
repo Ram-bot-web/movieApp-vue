54ee0b81df95cbf222c4d7d73ac09a77c307b737
Ramprasanthnambi <ramprasanth838@gmail.com>
2024-08-26 10:24:05 +0530

    Swiper impl

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <div class="feature-card">
+    <!-- <div class="feature-card">
       <router-link to="/movie/tt0409591">
         <img
           alt="Naruto Poster"
@@ -16,21 +16,76 @@
           </p>
         </div>
       </router-link>
-    </div>
+    </div> -->
+
+    <swiper
+      :effect="'coverflow'"
+      :grabCursor="true"
+      :centeredSlides="true"
+      :slidesPerView="'auto'"
+      :coverflowEffect="{
+        rotate: 0,
+        stretch: 0,
+        depth: 300,
+        modifier: 1,
+        // slideShadows: true,
+      }"
+      :autoplay="{
+        delay: 2500,
+        disableOnInteraction: false,
+      }"
+      :pagination="false"
+      :modules="modules"
+      :navigation="true"
+      :loop="true"
+      class="mySwiper"
+    >
+      <swiper-slide>
+        <img src="@/assets/Naruto1.jpg" />
+        <div>
+          <h1>Naruto</h1>
+        </div> </swiper-slide
+      ><swiper-slide
+        ><img
+          src="https://swiperjs.com/demos/images/nature-2.jpg" /></swiper-slide
+      ><swiper-slide
+        ><img
+          src="https://swiperjs.com/demos/images/nature-3.jpg" /></swiper-slide
+      ><swiper-slide
+        ><img
+          src="https://swiperjs.com/demos/images/nature-4.jpg" /></swiper-slide
+      ><swiper-slide
+        ><img
+          src="https://swiperjs.com/demos/images/nature-5.jpg" /></swiper-slide
+      ><swiper-slide
+        ><img
+          src="https://swiperjs.com/demos/images/nature-6.jpg" /></swiper-slide
+      ><swiper-slide
+        ><img
+          src="https://swiperjs.com/demos/images/nature-7.jpg" /></swiper-slide
+      ><swiper-slide
+        ><img
+          src="https://swiperjs.com/demos/images/nature-8.jpg" /></swiper-slide
+      ><swiper-slide
+        ><img src="https://swiperjs.com/demos/images/nature-9.jpg"
+      /></swiper-slide>
+    </swiper>
 
     <form @submit.prevent="SearchMovies()" class="search-box">
       <input type="text" placeholder="Search Movie" v-model="search" />
       <input type="submit" value="Search" />
     </form>
 
-    <div class="movie-list">
-      <div class="movie" v-for="movie in movies" :key="movie?.imdbID">
-        <router-link :to="`/movie/${movie.imdbID}`" class="movie-link">
+    <div class="movies-list">
+      <div class="movie" v-for="movie in movies" :key="movie.imdbID">
+        <router-link :to="'/movie/' + movie.imdbID" class="movie-link">
           <div class="product-image">
-            <img :src="movie?.Poster" alt="Movie Poster" />
-            <div class="type">{{ movie?.Type }}</div>
-            <div class="detail"> {{ movie?.Year }}</div>
-            <h3>{{ movie?.Title }}</h3>
+            <img :src="movie.Poster" alt="Movie Poster" />
+            <div class="type">{{ movie.Type }}</div>
+          </div>
+          <div class="detail">
+            <p class="year">{{ movie.Year }}</p>
+            <h3>{{ movie.Title }}</h3>
           </div>
         </router-link>
       </div>
@@ -42,8 +97,26 @@
 // @ is an alias to /src
 import env from "@/env";
 import { ref } from "vue";
+import { Swiper, SwiperSlide } from "swiper/vue";
 
+// Import Swiper styles
+import "swiper/css";
+
+import "swiper/css/effect-coverflow";
+import "swiper/css/pagination";
+// import required modules
+import {
+  EffectCoverflow,
+  Pagination,
+  Autoplay,
+  Navigation,
+} from "swiper/modules";
+// import "./style.css";
 export default {
+  components: {
+    Swiper,
+    SwiperSlide,
+  },
   setup() {
     const search = ref("");
     const movies = ref([]);
@@ -64,6 +137,7 @@ export default {
       search,
       movies,
       SearchMovies,
+      modules: [Autoplay, EffectCoverflow, Pagination, Navigation],
     };
   },
 };
@@ -74,7 +148,7 @@ export default {
   .feature-card {
     position: relative;
 
-    .feature-img {
+    .featured-img {
       display: block;
       width: 100%;
       height: 300px;
@@ -89,18 +163,18 @@ export default {
       left: 0;
       right: 0;
       bottom: 0;
-      background-color: rgba($color: #000000, $alpha: 0.6);
+      background-color: rgba(0, 0, 0, 0.6);
       padding: 16px;
       z-index: 1;
-    }
 
-    h3 {
-      color: #fff;
-      margin: 16px;
-    }
+      h3 {
+        color: #fff;
+        margin-bottom: 16px;
+      }
 
-    p {
-      color: #fff;
+      p {
+        color: #fff;
+      }
     }
   }
 
@@ -117,42 +191,122 @@ export default {
       border: none;
       outline: none;
       background: none;
-    }
 
-    input[type="text"] {
-      width: 100%;
-      color: #fff;
-      background-color: #496583;
-      font-size: 20px;
-      padding: 10px 16px;
-      border-radius: 8px;
-      margin-bottom: 15px;
-      transition: 0.4s;
+      &[type="text"] {
+        width: 100%;
+        color: #fff;
+        background-color: #496583;
+        font-size: 20px;
+        padding: 10px 16px;
+        border-radius: 8px;
+        margin-bottom: 15px;
+        transition: 0.4s;
 
-      &::placeholder {
-        color: #f3f3f3;
+        &::placeholder {
+          color: #f3f3f3;
+        }
+
+        &:focus {
+          box-shadow: 0px 3px 6px rgba(0, 0, 0, 0.2);
+        }
       }
 
-      &:focus {
-        box-shadow: 0px 3px 6px rgba($color: #000000, $alpha: 0.2);
+      &[type="submit"] {
+        width: 100%;
+        max-width: 300px;
+        background-color: #42b883;
+        padding: 16px;
+        border-radius: 8px;
+        color: #fff;
+        font-size: 20px;
+        text-transform: uppercase;
+        transition: 0.4s;
+
+        &:active {
+          background-color: #3b8070;
+        }
       }
     }
+  }
 
-    input[type="submit"] {
-      width: 100%;
-      max-width: 300px;
-      background-color: #42b883;
-      padding: 16px;
-      border-radius: 8px;
-      color: #fff;
-      font-size: 20px;
-      text-transform: uppercase;
-      transition: 0.4s;
+  .movies-list {
+    display: flex;
+    flex-wrap: wrap;
+    margin: 0px 8px;
 
-      &:active {
-        background-color: #3b8070;
+    .movie {
+      max-width: 50%;
+      flex: 1 1 50%;
+      padding: 16px 8px;
+
+      .movie-link {
+        display: flex;
+        flex-direction: column;
+        height: 100%;
+
+        .product-image {
+          position: relative;
+          display: block;
+
+          img {
+            display: block;
+            width: 100%;
+            height: 275px;
+            object-fit: cover;
+          }
+
+          .type {
+            position: absolute;
+            padding: 8px 16px;
+            background-color: #42b883;
+            color: #fff;
+            bottom: 16px;
+            left: 0px;
+            text-transform: capitalize;
+          }
+        }
+
+        .detail {
+          background-color: #496583;
+          padding: 16px 8px;
+          flex: 1 1 100%;
+          border-radius: 0px 0px 8px 8px;
+
+          .year {
+            color: #aaa;
+            font-size: 14px;
+          }
+
+          h3 {
+            color: #fff;
+            font-weight: 600;
+            font-size: 18px;
+          }
+        }
       }
     }
+  }
+
+  .swiper {
+    width: 100%;
+    padding-top: 50px;
+    padding-bottom: 50px;
+  }
+
+  .swiper-slide {
+    background-position: center;
+    background-size: cover;
+    width: 800px;
+    height: 300px;
+  }
+
+  .swiper-slide img {
+    display: block;
+    width: 100%;
+  }
+
+  .home .swiper-slide {
+    width: 800px !important;
   }
 }
 </style>
